56c0d25efda7f1279c1afd7a1352679b7c5c1564
Andy Grunwald <andygrunwald@gmail.com>
2013-04-19 17:02:32 +0200

    Switches whitespace chars to tabs

--- Chef/roles/web.rb
+++ Chef/roles/web.rb
@@ -5,7 +5,7 @@ run_list "recipe[python]"
 
 override_attributes(
 	:python => {
-    	:version => "2.7.4",
-    	:checksum => "62704ea0f125923208d84ff0568f7d50"
+		:version => "2.7.4",
+		:checksum => "62704ea0f125923208d84ff0568f7d50"
 	}
 )
