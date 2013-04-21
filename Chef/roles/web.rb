d6a39ac8ca72a81f2554bdabefb84435c8f042f6
Andy Grunwald <andygrunwald@gmail.com>
2013-04-21 14:08:02 +0200

    Added MySQL server to vagrant setup

--- Chef/roles/web.rb
+++ Chef/roles/web.rb
@@ -1,4 +1,18 @@
 name "web"
 description "The web role. Installs the required web stack"
 
-run_list "recipe[python]"
+run_list "recipe[python]",
+		 "recipe[mysql::server]",
+		 "recipe[mysql::client]"
+
+override_attributes(
+	:mysql => {
+		:remove_test_database => true,
+		:server_root_password => '',
+		:server_debian_password => '',
+		:server_repl_password => '',
+		:tunable => {
+			:max_allowed_packet => "50M"
+		}
+	}
+)
