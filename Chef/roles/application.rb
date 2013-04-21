b601fb01b137529129a5514d2b26d1b0b27b8759
Andy Grunwald <andygrunwald@gmail.com>
2013-04-21 18:00:09 +0200

    Added RabbitMQ to Chef installation

--- Chef/roles/application.rb
+++ Chef/roles/application.rb
@@ -2,7 +2,9 @@ name "application"
 description "The application role. Installs the required application stack"
 
 run_list "recipe[repositoryhandler]",
-		 "recipe[cvsanaly]"
+		 "recipe[cvsanaly]",
+		 "recipe[rabbitmq]",
+		 "recipe[rabbitmq::mgmt_console]"
 
 override_attributes(
 	:repositoryhandler => {
