059be7cfafd016b9e863af772d49beefb8060ee8
Andy Grunwald <andygrunwald@gmail.com>
2013-04-21 19:18:05 +0200

    Added PHP stack to V; using Chef

--- Chef/roles/web.rb
+++ Chef/roles/web.rb
@@ -3,7 +3,14 @@ description "The web role. Installs the required web stack"
 
 run_list "recipe[python]",
 		 "recipe[mysql::server]",
-		 "recipe[mysql::client]"
+		 "recipe[mysql::client]",
+		 "recipe[php]",
+		 "recipe[php::module_apc]",
+		 "recipe[php::module_curl]",
+		 "recipe[php::module_memcache]",
+		 "recipe[php::module_mysql]",
+		 "recipe[jolicode-php::ext-intl]",
+		 "recipe[jolicode-php::ext-pdo]"
 
 override_attributes(
 	:mysql => {
@@ -16,6 +23,11 @@ override_attributes(
 		:server_repl_password => '',
 		:tunable => {
 			:max_allowed_packet => "50M"
+		}
+	},
+	:php => {
+		:directives => {
+			'date.timezone' => 'Europe/Berlin'
 		}
 	}
 )
