a12fab6252eeb6f6aef126004effb7dae5c974a4
Andy Grunwald <andygrunwald@gmail.com>
2013-05-24 20:50:16 +0200

    Added Gitweb crawler as Command and Consumer

---
+++ Application/src/TYPO3Analysis/Command/GitwebCommand.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * @todo adds a description (license text, description of this class / file, etc)
+ */
+namespace TYPO3Analysis\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+use TYPO3Analysis\Helper\MessageQueue;
+
+class GitwebCommand extends Command {
+
+    /**
+     * Message Queue Queue
+     *
+     * @var string
+     */
+    const QUEUE = 'crawler.gitweb';
+
+    /**
+     * Message Queue routing
+     *
+     * @var string
+     */
+    const ROUTING = 'crawler.gitweb';
+
+    /**
+     * Config
+     *
+     * @var array
+     */
+    protected $config = array();
+
+    /**
+     * MessageQueue connection
+     *
+     * @var \TYPO3Analysis\Helper\MessageQueue
+     */
+    protected $messageQueue = null;
+
+    /**
+     * Project
+     *
+     * @var string
+     */
+    protected $project = null;
+
+    /**
+     * Configures the current command.
+     *
+     * @return void
+     */
+    protected function configure() {
+        $this->setName('crawler:gitweb')
+             ->setDescription('Generic task for message queue consumer')
+             ->addOption('project', null, InputOption::VALUE_OPTIONAL, 'Chose the project (for configuration, etc.).', 'TYPO3');
+    }
+
+    /**
+     * Returns the current project
+     *
+     * @return string
+     */
+    protected function getProject() {
+        return $this->project;
+    }
+
+    /**
+     * Sets the current project
+     *
+     * @param string    $project
+     * @return void
+     */
+    protected function setProject($project) {
+        $this->project = $project;
+    }
+
+    /**
+     * Initializes the command just after the input has been validated.
+     *
+     * Sets up the project, config, database and message queue
+     *
+     * @param InputInterface    $input    An InputInterface instance
+     * @param OutputInterface   $output   An OutputInterface instance
+     * @return void
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->setProject($input->getOption('project'));
+        $this->config = Yaml::parse(CONFIG_FILE);
+
+        $this->messageQueue = $this->initializeMessageQueue($this->config);
+    }
+
+    /**
+     * Initialize the message queue object
+     *
+     * @param array     $parsedConfig
+     * @return MessageQueue
+     */
+    private function initializeMessageQueue($parsedConfig) {
+        $config = $parsedConfig['RabbitMQ'];
+        $messageQueue = new MessageQueue($config['Host'], $config['Port'], $config['Username'], $config['Password'], $config['VHost']);
+
+        return $messageQueue;
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * Initialize and starts a single consumer.
+     *
+     * @param InputInterface    $input      An InputInterface instance
+     * @param OutputInterface   $output     An OutputInterface instance
+     * @return null|integer null or 0 if everything went fine, or an error code
+     * @throws \Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $projectConfig = $this->config['Projects'][$this->getProject()];
+
+        // If we do not get a gitweb, exit here
+        if (isset($projectConfig['Gitweb']) === false) {
+            return null;
+        }
+
+        $gitwebUrl = $projectConfig['Gitweb'];
+        if (filter_var($gitwebUrl, FILTER_VALIDATE_URL) === false) {
+            throw new \Exception('"' . $gitwebUrl . '" seems to be not a valid url', 1369417543);
+        }
+
+        $message = array(
+            'project' => $this->getProject(),
+            'url' => $gitwebUrl
+        );
+
+        $this->messageQueue->sendMessage($message, $projectConfig['RabbitMQ']['Exchange'], self::QUEUE, self::ROUTING);
+        return null;
+    }
+}
---
+++ Application/src/TYPO3Analysis/Consumer/Crawler/Gitweb.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * @todo adds a description (license text, description of this class / file, etc)
+ */
+namespace TYPO3Analysis\Consumer\Crawler;
+
+use TYPO3Analysis\Consumer\ConsumerAbstract;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Gitweb extends ConsumerAbstract {
+
+    /**
+     * HTTP Client
+     *
+     * @var \Buzz\Browser
+     */
+    protected $browser = null;
+
+    /**
+     * Gets a description of the consumer
+     *
+     * @return string
+     */
+    public function getDescription() {
+        return 'Crawls a Gitweb-Index page for Git-repositories';
+    }
+
+    /**
+     * Initialize the consumer.
+     * Sets the queue and routing key
+     *
+     * @return void
+     */
+    public function initialize() {
+        $this->setQueue('crawler.gitweb');
+        $this->setRouting('crawler.gitweb');
+
+        $curlClient = new \Buzz\Client\Curl();
+        $curlClient->setVerifyPeer(false);
+        $curlClient->setIgnoreErrors(true);
+        $this->browser = new \Buzz\Browser($curlClient);
+    }
+
+    /**
+     * The logic of the consumer
+     *
+     * @param \stdClass     $message
+     * @throws \Exception
+     */
+    public function process($message) {
+        $messageData = json_decode($message->body);
+
+        $content = $this->getContent($this->browser, $messageData->url);
+
+        $crawler = new Crawler($content);
+        /* @var $crawler \Symfony\Component\DomCrawler\Crawler */
+
+        $projectLinks = $crawler->filterXPath('//table[@class="project_list"]/tr/td[1]/a[@class="list"]');
+        foreach ($projectLinks as $node) {
+            $name = $node->nodeValue;
+
+            $href = $node->getAttribute('href');
+            $detailUrl = rtrim($messageData->url, '/') . $href;
+            $content = $this->getContent($this->browser, $detailUrl);
+
+            $detailCrawler = new Crawler($content);
+            /* @var $detailCrawler \Symfony\Component\DomCrawler\Crawler */
+            $gitUrl = $detailCrawler->filterXPath('//table[@class="projects_list"]/tr[@class="metadata_url"]/td[2]')->text();
+
+            $gitwebRecord = $this->getGitwebFromDatabase($gitUrl);
+            if ($gitwebRecord === false) {
+                $this->insertGitwebRecord($name, $gitUrl);
+            } else {
+                $this->getLogger()->info('Gitweb record already exists', array('git' => $gitUrl));
+            }
+
+            $this->addFurtherMessageToQueue($messageData->project, $gitUrl);
+        }
+
+        $this->acknowledgeMessage($message);
+    }
+
+    /**
+     * Returns the content of a web page
+     *
+     * @param \Buzz\Browser     $browser
+     * @param string            $url
+     * @return mixed
+     * @throws \Exception
+     */
+    private function getContent($browser, $url) {
+        $this->getLogger()->info('Requesting url', array('url' => $url));
+        $response = $browser->get($url);
+        /* @var $response \Buzz\Message\Response */
+
+        if ($response->getStatusCode() !== 200) {
+            $context = array(
+                'url' => $url,
+                'statusCode' => $response->getStatusCode()
+            );
+            $this->getLogger()->error('URL is not crawlable', $context);
+            $exceptionMessage = sprintf('URL "%s" is not crawlable', $url);
+            throw new \Exception($exceptionMessage, 1369417933);
+        }
+
+        return $response->getContent();
+    }
+
+    /**
+     * Adds new messages to queue system to download the git repository
+     *
+     * @param string    $project
+     * @param string    $repository
+     * @return void
+     */
+    private function addFurtherMessageToQueue($project, $repository) {
+        $message = array(
+            'project' => $project,
+            'git' => $repository,
+        );
+
+        $this->getMessageQueue()->sendMessage($message, 'TYPO3', 'download.git', 'download.git');
+    }
+
+    /**
+     * Receives a single gitweb record of the database
+     *
+     * @param string   $repository
+     * @return bool|array
+     */
+    private function getGitwebFromDatabase($repository) {
+        $fields = array('id');
+        $rows = $this->getDatabase()->getRecords($fields, 'gitweb', array('git' => $repository), '', '', 1);
+
+        $row = false;
+        if (count($rows) === 1) {
+            $row = array_shift($rows);
+            unset($rows);
+        }
+
+        return $row;
+    }
+
+    /**
+     * Inserts a new gitweb record to database
+     *
+     * @param string   $name
+     * @param string   $repository
+     * @return void
+     */
+    private function insertGitwebRecord($name, $repository) {
+        $data = array(
+            'name' => $name,
+            'git' => $repository
+        );
+        $this->getDatabase()->insertRecord('gitweb', $data);
+        $this->getLogger()->info('Inserted new gitweb record', $data);
+    }
+}
