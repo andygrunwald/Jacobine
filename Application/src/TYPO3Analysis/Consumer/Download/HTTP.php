886babda9cd3d0a0f6fa6354d1b283096ea4f55e
Andy Grunwald <andygrunwald@gmail.com>
2013-06-22 09:04:26 +0200

    Refactored Download\\HTTP Consumer to respect always downloaded files

--- Application/src/TYPO3Analysis/Consumer/Download/HTTP.php
+++ Application/src/TYPO3Analysis/Consumer/Download/HTTP.php
@@ -58,49 +58,54 @@ class HTTP extends ConsumerAbstract {
         // @todo find a better way for the filename ... Download-Prefix in project config?
         // in $messageData->Project you can find the project
         $fileName = 'typo3_' . $record['version'] . '.tar.gz';
-        $targetFile = $targetTempDir . $fileName;
-
-        $context = array(
-            'downloadUrl' => $record['url_tar'],
-            'targetFile' => $targetFile
-        );
-        $this->getLogger()->info('Start download', $context);
-
-        // We download the file with wget, because we get a progress bar for free :)
-        $command = 'wget ' . escapeshellarg($record['url_tar']) . ' --output-document=' . escapeshellarg($targetFile);
-        $this->executeCommand($command);
-
-        // If there is no file after download, exit here
-        if (file_exists($targetFile) !== true) {
-            $this->getLogger()->critical('File does not exist after download', array('targetFile' => $targetFile));
-            $msg = sprintf('File %s does not exist after download', $targetFile);
-            throw new \Exception($msg, 1366829810);
-        }
+        $targetTempFile = $targetTempDir . $fileName;
 
         $config = $this->getConfig();
         $projectConfig = $config['Projects'][$messageData->project];
         $targetDir = rtrim($projectConfig['ReleasesPath'], DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        $targetFile = $targetDir . $fileName;
+
+        // If the file already there do not download it again
+        if (file_exists($targetTempFile) === true && $record['checksum_tar_md5'] && md5_file($targetFile) === $record['checksum_tar_md5']) {
+            $this->setVersionAsDownloadedInDatabase($record['id']);
+            $this->acknowledgeMessage($message);
+            $this->addFurtherMessageToQueue($messageData->project, $record['id'], $targetFile);
+            return;
+        }
+
+        $context = array(
+            'downloadUrl' => $record['url_tar'],
+            'targetFile' => $targetTempFile
+        );
+        $this->getLogger()->info('Start download', $context);
+
+        // We download the file with wget, because we get a progress bar for free :)
+        $command = 'wget ' . escapeshellarg($record['url_tar']) . ' --output-document=' . escapeshellarg($targetTempFile);
+        $this->executeCommand($command);
+
+        // If there is no file after download, exit here
+        $this->checkIfFileExistsAfterDownload($targetTempFile);
 
         if (is_dir($targetDir) === false) {
             mkdir($targetDir, 0777, true);
         }
 
-        rename($targetFile, $targetDir . $fileName);
+        rename($targetTempFile, $targetFile);
 
         // If the hashes are not equal, exit here
-        $md5Hash = md5_file($targetDir . $fileName);
+        $md5Hash = md5_file($targetFile);
         if ($record['checksum_tar_md5'] && $md5Hash !== $record['checksum_tar_md5']) {
 
             $msg = 'Checksums for file are not equal';
             $context = array(
-                'targetFile' => $targetDir . $fileName,
+                'targetFile' => $targetFile,
                 'databaseHash' => $record['checksum_tar_md5'],
                 'fileHash' => $md5Hash
             );
             $this->getLogger()->critical($msg, $context);
 
             $msg = 'Checksums for file "%s" are not equal (Database: %s, File hash: %s)';
-            $msg = sprintf($msg, $targetDir . $fileName, $record['checksum_tar_md5'], $md5Hash);
+            $msg = sprintf($msg, $targetFile, $record['checksum_tar_md5'], $md5Hash);
             throw new \Exception($msg, 1366830113);
         }
 
@@ -110,7 +115,7 @@ class HTTP extends ConsumerAbstract {
         $this->acknowledgeMessage($message);
 
         // Adds new messages to queue: extract the file, get filesize or tar.gz file
-        $this->addFurtherMessageToQueue($messageData->project, $record['id'], $targetDir . $fileName);
+        $this->addFurtherMessageToQueue($messageData->project, $record['id'], $targetFile);
     }
 
     /**
@@ -160,5 +165,13 @@ class HTTP extends ConsumerAbstract {
     private function setVersionAsDownloadedInDatabase($id) {
         $this->getDatabase()->updateRecord('versions', array('downloaded' => 1), array('id' => $id));
         $this->getLogger()->info('Set version as downloaded', array('versionId' => $id));
+    }
+
+    private function checkIfFileExistsAfterDownload($targetFile) {
+        if (file_exists($targetFile) !== true) {
+            $this->getLogger()->critical('File does not exist after download', array('targetFile' => $targetFile));
+            $msg = sprintf('File %s does not exist after download', $targetFile);
+            throw new \Exception($msg, 1366829810);
+        }
     }
 }
