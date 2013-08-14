1172e781a93205466af76623bf90697e1e29301a
Andy Grunwald <andygrunwald@gmail.com>
2013-08-14 22:17:39 +0200

    Installed vim

--- Chef/roles/base.rb
+++ Chef/roles/base.rb
@@ -3,7 +3,8 @@ description "The base role. Sets up basic stuff like apt, git etc."
 
 run_list "recipe[apt]",
 		 "recipe[build-essential]",
-		 "recipe[git]"
+		 "recipe[git]",
+		 "recipe[vim]"
 
 override_attributes(
 	:build_essential => {
