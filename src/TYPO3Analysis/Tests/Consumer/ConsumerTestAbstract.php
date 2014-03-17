d206446e6089a2eefb45296639ff7735775bf3cd
Andy Grunwald <andygrunwald@gmail.com>
2014-03-17 22:17:29 +0100

    Added some more unit tests for ConsumerAbstract

--- src/TYPO3Analysis/Tests/Consumer/ConsumerTestAbstract.php
+++ src/TYPO3Analysis/Tests/Consumer/ConsumerTestAbstract.php
@@ -50,4 +50,73 @@ abstract class ConsumerTestAbstract extends \PHPUnit_Framework_TestCase
         $this->assertInternalType('string', $routing);
         $this->assertTrue(strlen($routing) > 0);
     }
+
+    public function testMessageGetterAndSetter()
+    {
+        $this->assertNull($this->consumer->getMessage());
+
+        $message = new \stdClass();
+        $message->dummy = 'attribute';
+
+        $this->consumer->setMessage($message);
+
+        $this->assertSame($message, $this->consumer->getMessage());
+    }
+
+    public function testExchangeGetterAndSetter()
+    {
+        $this->assertEmpty($this->consumer->getExchange());
+
+        $exchange = 'EXCHANGE';
+        $this->consumer->setExchange($exchange);
+
+        $this->assertSame($exchange, $this->consumer->getExchange());
+    }
+
+    public function testConsumerTag()
+    {
+        $consumerTag = $this->consumer->getConsumerTag();
+
+        $this->assertInternalType('string', $consumerTag);
+        $this->assertNotEmpty($consumerTag);
+    }
+
+    public function testDatabaseGetterAndSetter()
+    {
+        $this->assertNull($this->consumer->getDatabase());
+
+        // Mock of \TYPO3Analysis\Helper\DatabaseFactory
+        $databaseFactoryMock = $this->getMock('TYPO3Analysis\Helper\DatabaseFactory');
+        $constructorArgs = [$databaseFactoryMock, '', '', '', '', ''];
+        $databaseMock = $this->getMock('\TYPO3Analysis\Helper\Database', [], $constructorArgs);
+
+        $this->consumer->setDatabase($databaseMock);
+
+        $this->assertSame($databaseMock, $this->consumer->getDatabase());
+    }
+
+    public function testMessageQueueGetterAndSetter()
+    {
+        $this->assertNull($this->consumer->getMessageQueue());
+
+        $amqpConnectionMock = $this->getMock('\PhpAmqpLib\Connection\AMQPConnection', [], [], '', false);
+        $amqpFactoryMock = $this->getMock('\TYPO3Analysis\Helper\AMQPFactory', ['createMessage']);
+        $constructorArgs = [$amqpConnectionMock, $amqpFactoryMock];
+        $messageQueueMock = $this->getMock('\TYPO3Analysis\Helper\MessageQueue', [], $constructorArgs);
+
+        $this->consumer->setMessageQueue($messageQueueMock);
+
+        $this->assertSame($messageQueueMock, $this->consumer->getMessageQueue());
+    }
+
+    public function testLoggerGetterAndSetter()
+    {
+        $this->assertNull($this->consumer->getLogger());
+
+        $loggerMock = $this->getMock('\Monolog\Logger', [], ['']);
+
+        $this->consumer->setLogger($loggerMock);
+
+        $this->assertSame($loggerMock, $this->consumer->getLogger());
+    }
 }
